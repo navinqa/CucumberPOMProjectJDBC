92490912a7ead503155d87361b38900d1aa1a7de
navinqa <162291832+navinqa@users.noreply.github.com>
2024-03-17 21:44:08 +0000

    Update HelloWord.java
    manual commit github

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -2,5 +2,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello word Local file changes 123");
         System.out.println("this is a commit feature/adms1703 to master");
+          System.out.println("this a direct changes in master github.com commit");
     }
 }
