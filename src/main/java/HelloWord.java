f7c72d1cbc3634fbbe357c1b3c2b8ad25d1ce1dd
navinqa <162291832+navinqa@users.noreply.github.com>
2024-03-17 22:12:24 +0000

    Update HelloWord.java
    Rebase command directly changes online

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -3,5 +3,7 @@ public class HelloWord {
         System.out.println("Hello word Local file changes 123");
         System.out.println("this is a commit feature/adms1703 to master");
           System.out.println("this a direct changes in master github.com commit");
+         System.out.println("demo rebase command directly changes in remote master github.com");
+        
     }
 }
